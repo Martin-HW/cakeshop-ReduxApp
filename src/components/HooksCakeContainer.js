d7250a40eb98c236c3c842e370fa917717a53d46
martin suarez <msuarez@webcreek.com>
2021-07-24 13:01:56 -0500

    globant commit

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { buyCake } from '../redux';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake } from "../redux";
 
 const HooksCakeContainer = () => {
-    const numOfCakes = useSelector(state => state.numOfCakes)
-    const dispatch = useDispatch(); 
-    
-    return (
-        <div>
-            <h2>Numero de pasteles - {numOfCakes}</h2>
-            <button onClick = {() => dispatch( buyCake() )}>Comprar pastel</button>
-        </div>
-    )
-}
+  const numOfCakes = useSelector((state) => state.numOfCakes);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Numero de pasteles - {numOfCakes}</h2>
+      <button onClick={() => dispatch(buyCake())}>Comprar pastel</button>
+    </div>
+  );
+};
 
 export default HooksCakeContainer;
